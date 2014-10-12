ea0b4df4f1b862630414efe50311c43ed96665e6
Alexandra Willis <awil294@lsu.edu>
2014-10-11 21:49:28 -0500

    Wrote Scanner
    it works now yey

--- scanner.java
+++ scanner.java
@@ -1,57 +1,34 @@
+// Scanner.java -- the implementation of class Scanner
+
 import java.io.*;
- 
+import java.lang.Math;
+
 class Scanner {
   private PushbackInputStream in;
-  private byte[] buf;
-  private String identifiers = "+-.*/<=>!?:$%_&~^";
+  private byte[] buf = new byte[1000];
+  private final String idents = "+-.*/<=>!?:$%_&~^";
   public Scanner(InputStream i) { in = new PushbackInputStream(i); }
-   
+    
   public Token getNextToken() {
-        buf = new byte[1000];
     int bite = -1;
-        int next = -1;
-    // It would be more efficient if we'd maintain our own input buffer
-    // and read characters out of that buffer, but reading individual
-    // characters from the input stream is easier.
+	
     try {
       bite = in.read();
     } catch (IOException e) {
       System.err.println("We fail: " + e.getMessage());
     }
- 
-    // TODO: skip white space and comments !!done!!
-   
-    if(bite == 32)  //32 is unicode value for whitespace. skip it.
-                return getNextToken();
-   
-    if(bite == 9) //9 is unicode value for tab. Trash.
-        return getNextToken();
-   
-    if(bite == 10) //10 is line feed unicode value. Trash.
-        return getNextToken();
-   
-    if(bite == 13) //13 is carriage return unicode value. Trash.
-        return getNextToken();
-   
-    if(bite == 12) //12 is form feed character unicode value. Trash.
-        return getNextToken();
-   
-    if(bite == 59) { //when it comes across a ; (value 59), read until end of line (value 10). Then return getNextToken again
-                while(bite != 10) {
-                        try {
-                              bite = in.read();
-                            } catch (IOException e) {
-                              System.err.println("We fail: " + e.getMessage());
-                            }          
-                }      
-                return getNextToken();
-        }
-       
+	
+	// Skip whitespace/tab/newline/return
+	
+	if( bite == 32 || bite == 9 || bite == 10 || bite == 12 || bite == 13 )
+		return getNextToken();
+
     if (bite == -1)
       return null;
- 
+
     char ch = (char) bite;
-       
+	
+	
     // Special characters
     if (ch == '\'')
       return new Token(Token.QUOTE);
@@ -60,101 +37,137 @@ class Scanner {
     else if (ch == ')')
       return new Token(Token.RPAREN);
     else if (ch == '.')
-      // We ignore the special identifier `...'.
       return new Token(Token.DOT);
- 
+
     // Boolean constants
     else if (ch == '#') {
       try {
-          bite = in.read();
+		bite = in.read();
       } catch (IOException e) {
-          System.err.println("We fail: " + e.getMessage());
+		System.err.println("We fail: " + e.getMessage());
       }
- 
+
       if (bite == -1) {
-        System.err.println("Unexpected EOF following #");
-        return null;
+	System.err.println("Unexpected EOF following #");
+	return null;
       }
       ch = (char) bite;
       if (ch == 't')
-        return new Token(Token.TRUE);
+	return new Token(Token.TRUE);
       else if (ch == 'f')
-        return new Token(Token.FALSE);
+	return new Token(Token.FALSE);
       else {
-        System.err.println("Illegal character '" + (char) ch + "' following #");
-        return getNextToken();
+	System.err.println("Illegal character '" + (char) ch + "' following #");
+	return getNextToken();
       }
     }
- 
+
     // String constants
     else if (ch == '"') {
-      // TODO: scan a string into the buffer variable buf
-        int counter = 0;
-        while(ch != '"'){
-                buf[counter] = (byte)ch;
-                counter++;
-                try {
-                        ch = (char) in.read();
-                    } catch (IOException e) {
-                      System.err.println("We fail: " + e.getMessage());
-                    }
-               
-        }
-      return new StrToken(new String(buf,0,counter));
+	  // counter to keep track of length of buf
+	  int numChars = 0;
+	  
+	  // read first char of string
+	  try {
+			ch = (char) in.read();
+      } catch (IOException e) {
+			System.err.println("We fail: " + e.getMessage());
+	  }
+	  
+      // while not reached end of string
+	  while(ch != '"'){
+		// continue to read the string
+		buf[numChars] = (byte) ch;
+		numChars ++;
+		try {
+			ch = (char) in.read();
+		} catch (IOException e) {
+			System.err.println("We fail: " + e.getMessage());
+		}
+		
+	  }
+	  
+	  return new StrToken(buf.toString());
     }
- 
+
     // Integer constants
     else if (ch >= '0' && ch <= '9') {
-      int i = ch - '0';
-      // TODO: scan the number and convert it to an integer
-      try {
-                        next = in.read();
-                    } catch (IOException e) {
-                      System.err.println("We fail: " + e.getMessage());
-                    }
-      while((char)next >= '0' && (char)next <= '9'){
-                i = i*10 + (char)next - '0';
-                try {
-                        next = in.read();
-                    } catch (IOException e) {
-                      System.err.println("We fail: " + e.getMessage());
-                    }
-      }
-      // put the character after the integer back into the input
+      int i = Character.getNumericValue(ch);
+      
+	  int[] digits = new int[1000];
+	  int count = 0;
+	  digits[count] = i;
+	  
+	  // while still digits left in the number
+	  while( ch >= '0' && ch <= '9' ){
+		// continue to read number
+		i = Character.getNumericValue(ch);
+		digits[count] = i;
+		count++;
+		
+		try {
+			ch = (char) in.read();
+		} catch (IOException e) {
+			System.err.println("We fail: " + e.getMessage());
+		}
+		
+	  }
+	  
+	  // convert digits into actual number
+	  int number = 0;
+	  for(int j=0; j<count; j++){
+		int cij = 10 ^ (count - 1 - j);
+		if( count - 1 - j == 0 )
+			number += digits[j];
+		else
+			number += digits[j] * ( Math.pow(10, count - 1 - j ) );
+	  }
+	  
+      /* put the character after the integer back into the input
       // in->putback(ch);
-      try {
-                        in.unread(next);
-                    } catch (IOException e) {
-                      System.err.println("We fail: " + e.getMessage());
-                    }
-      return new IntToken(i);
+	  try {
+			in.unread(ch);
+	  } catch (IOException e) {
+			System.err.println("We fail: " + e.getMessage());
+	  }*/
+	  
+	  
+      return new IntToken(number);
     }
- 
+
     // Identifiers
-    else if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a') && (ch <= 'z') || (identifiers.indexOf(ch) > -1)) {
-      // TODO: scan an identifier into the buffer
-        int counter = 0;
-        while((ch >= 'A' && ch <= 'Z') || (ch >= 'a') && (ch <= 'z') || (identifiers.indexOf(ch) > -1)){
-                buf[counter] = (byte)ch;
-                try {
-                        ch = (char) in.read();
-                    } catch (IOException e) {
-                      System.err.println("We fail: " + e.getMessage());
-                    }
-                counter++;
-        }
+    else if ( ch >= 'A' && ch <= 'Z' || (ch >= 'a') && (ch <= 'z')  || idents.contains(ch+"") ) {
+      
+	  int count = 0;
+	  try {
+			ch = (char) in.read();
+		} catch (IOException e) {
+			System.err.println("We fail: " + e.getMessage());
+		}
+	  buf[count] = (byte) ch; 
+	
+	  while( ch >= 'A' && ch <= 'Z' || (ch >= 'a') && (ch <= 'z') || idents.contains(ch+"") ){
+		
+		buf[count] = (byte) ch;
+		
+		try {
+			ch = (char) in.read();
+		} catch (IOException e) {
+			System.err.println("We fail: " + e.getMessage());
+		}
+		
+	  
+	  }
+	  
       // put the character after the identifier back into the input
       // in->putback(ch);
-         try {
-                        in.unread((byte)ch);
-                    } catch (IOException e) {
-                      System.err.println("We fail: " + e.getMessage());
-                    }
-      return new IdentToken(new String(buf, 0, counter));
-    } else {
-        // Illegal character
-        System.err.println("Illegal input character '" + (char) ch + '\'');
-        return getNextToken();
-    }  
+      return new IdentToken(buf.toString());
+    }
+
+    // Illegal character
+    else {
+      System.err.println("Illegal input character '" + (char) ch + '\'');
+      return getNextToken();
+    }
   };
 }
